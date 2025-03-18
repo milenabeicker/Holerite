ed20fc364bd07b1833546904d08123a868d6cc24
milenabeicker <milenakessia87@gmail.com>
2025-03-18 11:14:43 -0300

    Initial Commit

---
+++ Holerite/HoleriteApp.swift
@@ -0,0 +1,17 @@
+//
+//  HoleriteApp.swift
+//  Holerite
+//
+//  Created by Milena Beicker on 18/03/25.
+//
+
+import SwiftUI
+
+@main
+struct HoleriteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
